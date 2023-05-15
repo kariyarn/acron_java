5cd0fa939870d718ba13a1e2e418fc61a525ec36
kariyarn <kariyarn@hanmail.net>
2023-05-15 17:52:48 +0900

    step17 mainCalss12에 update, delete기능 추가함

---
+++ Step17_JDBC/src/test/main/MainClass12.java
@@ -0,0 +1,35 @@
+package test.main;
+
+import test.dao.MemberDao;
+import test.dto.MemberDto;
+
+public class MainClass12 {
+	public static void main(String[] args) {
+//		//추가할 회원의 정보
+//		String name = "덩어리";
+//		String addr = "목동";
+//		
+//		//추가할 회원의 정보를 MemberDto 객체에 담는다.
+//		MemberDto dto = new MemberDto();
+//		dto.setName(name);
+//		dto.setAddr(addr);
+//		//DB에 저장하기
+//		new MemberDao().insert(dto);
+//		
+//		//업데이트할 회원의 정보
+//		int num2 = 1;
+//		String name2 = "김형규";
+//		String addr2 = "경기광주";
+//		MemberDto dto2 = new MemberDto();
+//		dto2.setNum(num2);
+//		dto2.setName(name2);
+//		dto2.setAddr(addr2);
+//		new MemberDao().update(dto2);
+//		
+//		//삭제할 회원의 정보
+//		int num3 = 6;
+//		MemberDto dto3 = new MemberDto();
+//		dto3.setNum(num3);
+//		new MemberDao().delete(dto3);
+	}
+}
